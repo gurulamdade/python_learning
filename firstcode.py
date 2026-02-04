2ca5b08527ecb3634ebd750dca775dea9bf63062
guru20066 <lamdadeguru@gmail.com>
2026-02-04 22:20:34 +0530

    Day 1 Variables

--- firstcode.py
+++ firstcode.py
@@ -1,5 +1,13 @@
 # remote code line 04 feb 2026
 # updated on local branch 9pm 4 feb 2026
 
+signal_color = True # True when it is in Green, Red or Orange Booloean Datatype
+overs = 10.5 # Float Datatype - Decimal value 
+name_of_student = "Guru" # String Datatype - Text value
+subject_marks = 95 # Integer Datatype - Whole number value
 
-print("Hello, World!")
+
+print("signal_color:", signal_color)
+print("overs:", overs)
+print("name_of_student:", name_of_student)
+print("subject_marks:", subject_marks)
