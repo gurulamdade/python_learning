c2d395f7f57241bb07c9bc19136d6f1eb435b32b
GURU <gurulamdade2@gmail.com>
2026-02-08 19:54:20 +0530

    Declare a cricket player Record/profile

---
+++ player-record.py
@@ -0,0 +1,13 @@
+# Declare a cricket player Record/profile
+
+Strick_Rate = 140.33
+Total_runs = 7000
+Name_of_the_player = "ROHIT GURUNATH SHARMA"
+Country = "INDIA"
+isAllRounder = "False"
+
+print("Strick_Rate = " ,Strick_Rate)
+print("Total_runs = " ,Total_runs)
+print("Name_of_the_player = " ,Name_of_the_player)
+print("Country = " ,Country)
+print("isAllRounder = " ,isAllRounder)
