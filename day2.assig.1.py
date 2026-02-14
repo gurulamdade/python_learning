993559a3605cb73d1631e2e0dd4304418a46d358
GURU <gurulamdade2@gmail.com>
2026-02-14 14:54:04 +0530

    assigment 1

---
+++ day2.assig.1.py
@@ -0,0 +1,20 @@
+strick_rate = 140.33
+total_runs = 7000
+name_of_the_players = "ROHIT GURUNATH SHARMA"
+country = "INDIA"
+isAllrounder = "false"
+
+print("---------Memory Reference identification------")
+print("strick_rate=",id(strick_rate))
+print("total_runs=",id(total_runs))
+print("name_of_the_players=",id(name_of_the_players))
+print("country=",id(country))
+print("isAllrounder=",id(isAllrounder))
+
+print("-----------------------------------------------------------------")
+print("---------Type identification---------------")
+print("Type of strick_rate:",type(strick_rate))
+print("Type of total_runs:",type(total_runs))
+print("Type of name_of_the_players:",type(name_of_the_players))
+print("Type of country:",type(country))
+print("Type of isAllrounder:",type(isAllrounder))
