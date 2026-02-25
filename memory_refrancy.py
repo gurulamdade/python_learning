5940a6ead0e403c228d2fee7474d3849b1e2adb8
GURU <gurulamdade2@gmail.com>
2026-02-25 23:34:21 +0530

    Add memory reference example with variable types and addresses

---
+++ memory_refrancy.py
@@ -0,0 +1,11 @@
+a = 10
+b = 10
+
+print("Value of a:", a)
+print("Value of b:", b)
+
+print("Memory address of a:", id(a))
+print("Memory address of b:", id(b))
+
+print("Type of a:", type(a))
+print("Type of b:", type(b))
